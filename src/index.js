af83a8ed7c3821f7bb1da9d428c149b5b4f7fd4b
Ivan Silva <ivankuartz@hotmail.com>
2022-04-05 19:02:02 -0300

    Changed implementation of react-render to createRoot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
 import store from './redux/configureStore';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
