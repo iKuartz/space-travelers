e74420754bec9f9ea1571ab3ba43b494c241281b
Ivan Silva <ivankuartz@hotmail.com>
2022-04-06 12:43:27 -0300

    Added mission fetch action and action creators and reducer

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,2 +1,2 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
---
+++ src/redux/missions/Missions.js
@@ -0,0 +1,26 @@
+import { FETCH_MISSIONS } from '../actionTypes';
+
+const fetchMissions = () => async (dispatch) => {
+  const response = await fetch('https://api.spacexdata.com/v3/missions');
+  const data = await response.json();
+  dispatch({ type: FETCH_MISSIONS, data });
+};
+
+function missionsReducer(state = [], action) {
+  const missionList = [];
+  switch (action.type) {
+    default: return state;
+
+    case FETCH_MISSIONS:
+      action.data.forEach((mission) => {
+        missionList.push({
+          id: mission.mission_id,
+          name: mission.mission_name,
+          type: mission.mission_type,
+        });
+      });
+      return missionList;
+  }
+}
+
+export { fetchMissions, missionsReducer };
