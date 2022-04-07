648833a9396d8dca373d806609995f08b987b1fe
Ivan Silva <ivankuartz@hotmail.com>
2022-04-07 14:06:07 -0300

    Implemented mission leaving and joining

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchMissions } from '../redux/missions/Missions';
+import {
+  fetchMissions,
+  leaveMission,
+  missionsReducer,
+} from "../redux/missions/Missions";
 
 export default function Missions() {
   const dispatch = useDispatch();
@@ -9,13 +13,32 @@ export default function Missions() {
     dispatch(fetchMissions());
   }, []);
   const missions = useSelector((state) => state.missions);
+
+  const handleJoining = (id) => {
+    dispatch(bookMission(id));
+  };
+
+  const handleCanceling = (id) => {
+    dispatch(cancelMission(id));
+  };
+
   return (
     <div>
       {missions.map((mission) => (
         <div key={mission.id}>
           <h2>{mission.name}</h2>
+          {mission.joined && <p>Joined!</p>}
           <p>{mission.description}</p>
-          <input type="checkbox" checked="{mission.reserved}" />
+          <input type='checkbox' checked='{mission.reserved}' />
+          {mission.joined ? (
+            <button onClick={() => handleCanceling(mission.id)} type='button'>
+              Leave mission
+            </button>
+          ) : (
+            <button onClick={() => handleJoining(mission.id)} type='button'>
+              Join mission
+            </button>
+          )}
         </div>
       ))}
     </div>
