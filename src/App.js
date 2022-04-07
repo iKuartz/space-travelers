2ff8f9af3cd27e6e53948938c6330c0aaa79cb3d
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-04-07 12:32:25 -0500

    Rendered components in the page, adding route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
-// import Profiles from './components/Profiles';
+import Profiles from './components/Profiles';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
+        <Route path="/profiles" element={<Profiles />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
 export default function Profiles() {
   const bookedRockets = useSelector((state) => state.rockets).filter(
@@ -6,5 +6,30 @@ export default function Profiles() {
   );
   const bookedMissions = useSelector((state) => state.missions).filter(
     (mission) => mission.joined,
+  );
+
+  return (
+    <div>
+      <div>
+        <h2>My Missions</h2>
+        <ul>
+          {bookedMissions.map((mission) => (
+            <li key={mission.id}>
+              {mission.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>My Rockets</h2>
+        <ul>
+          {bookedRockets.map((rocket) => (
+            <li key={rocket.id}>
+              {rocket.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
