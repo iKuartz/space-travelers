8b959a6cb404e227fb1a989b93c20f987780f5f5
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-04-08 10:50:52 -0500

    Adding header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
 
+import './Header.css';
+
 import logo from '../assets/planet.png';
 
 export default function Header() {
   return (
     <nav>
-      <div>
-        <img src={logo} alt="Planet logo" />
+      <div className="title">
+        <img className="logo" src={logo} alt="Planet logo" />
         <h1>Space Travelers&apos; Hub</h1>
       </div>
-      <ul>
+      <ul className="navLinks">
         <li>
           <Link to="/">
             Rockets
