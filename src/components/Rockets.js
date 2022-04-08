4712630c6fc37bef7e412963adb2a811d222bc7c
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-04-08 11:48:15 -0500

    Adding most rocket styling

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { fetchRockets, bookRocket, cancelRocket } from '../redux/rockets/Rockets';
+
+import './Rocket.css';
 
 export default function Rockets() {
   const dispatch = useDispatch();
@@ -17,21 +18,31 @@ export default function Rockets() {
   };
 
   return (
-    <div>
+    <div className="rocketContainer">
       {rockets.map((rocket) => (
-        <div key={rocket.id}>
-          <img src={rocket.img} alt={`rocket named: ${rocket.name}`} />
-          <h1>{rocket.name}</h1>
-          <p>
-            {rocket.booked && 'Booked'}
-            {' '}
-            {rocket.description}
-          </p>
-          <p>
-            type:
-            {rocket.type}
-          </p>
-          {rocket.booked ? <button onClick={() => handleCanceling(rocket.id)} type="button">Cancel reservation</button> : <button onClick={() => handleBooking(rocket.id)} type="button">Reserve rocket</button>}
+        <div key={rocket.id} className="rocket">
+          <img
+            className="rocketImg"
+            src={rocket.img}
+            alt={`rocket named: ${rocket.name}`}
+          />
+          <div className="rocketTxt">
+            <h1>{rocket.name}</h1>
+            <p>
+              {rocket.booked && <i>Reserved</i>}
+              {' '}
+              {rocket.description}
+            </p>
+            {rocket.booked ? (
+              <button className="btn rocketButton" onClick={() => handleCanceling(rocket.id)} type="button">
+                Cancel reservation
+              </button>
+            ) : (
+              <button className="btn rocketButton" onClick={() => handleBooking(rocket.id)} type="button">
+                Reserve rocket
+              </button>
+            )}
+          </div>
         </div>
       ))}
     </div>
